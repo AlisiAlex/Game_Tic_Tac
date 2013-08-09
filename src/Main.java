bd999b8b90b7904057d77abccb0bc3f0b5447def
EvAlexVi <AlisiAlexEv@outlook.com>
2013-08-09 22:58:54 +0400

    Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Game.MechanicsGame;
+
+public class Main {
+    public static void main(String[] args){
+        MechanicsGame mechanicsGame = new MechanicsGame();
+        mechanicsGame.Game();
+    }
+}
